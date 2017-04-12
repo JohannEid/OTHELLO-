3fd9b2335624f462214eada79869aeb0a419c795
Clemzoo <clement.de-bailliencourt@edu.ece.fr>
2017-04-12 18:07:01 +0200

    Begin to implement graphic design with SFML library

--- cmake-build-debug/CMakeFiles/Othello.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/Othello.dir/DependInfo.cmake
@@ -4,18 +4,19 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/johann/Work/Othello/Board.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/Board.cpp.o"
-  "/home/johann/Work/Othello/Game.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/Game.cpp.o"
-  "/home/johann/Work/Othello/Node.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/Node.cpp.o"
-  "/home/johann/Work/Othello/Pawn.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/Pawn.cpp.o"
-  "/home/johann/Work/Othello/Player.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/Player.cpp.o"
-  "/home/johann/Work/Othello/Tree.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/Tree.cpp.o"
-  "/home/johann/Work/Othello/main.cpp" "/home/johann/Work/Othello/cmake-build-debug/CMakeFiles/Othello.dir/main.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/Board.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/Board.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/Game.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/Game.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/Node.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/Node.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/Pawn.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/Pawn.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/Player.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/Player.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/Tree.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/Tree.cpp.o"
+  "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/main.cpp" "/Users/clementdebailliencourt/CLionProjects/OTHELLO-/cmake-build-debug/CMakeFiles/Othello.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/SFML.framework"
   )
 
 # Targets to which this target links.
